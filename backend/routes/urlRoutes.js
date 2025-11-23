a9fef38f5c170cfde655e49334227cbfba83a725
AnilSajjanshetty <anilsajjanshetty112@gmail.com>
2025-11-23 12:07:05 +0530

    chore:removed comments.

--- backend/routes/urlRoutes.js
+++ backend/routes/urlRoutes.js
@@ -1,26 +1,3 @@
-// // routes/urlRoutes.js
-// import express from "express";
-// import {
-//   createShortUrl,
-//   getAllUrls,
-//   getUrlDetails,
-//   deleteUrl,
-//   redirectShortUrl,
-// } from "../controllers/urlController.js";
-
-// const router = express.Router();
-
-// // CRUD API
-// router.post("/create", createShortUrl);
-// router.get("/all", getAllUrls);
-// router.get("/details/:code", getUrlDetails);
-// router.delete("/delete/:code", deleteUrl);
-
-// // Redirect route (should be last)
-// router.get("/:code", redirectShortUrl);
-
-// export default router;
-
 import express from "express";
 import {
   createLink,
